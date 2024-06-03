e32ff6368d1f8ba7d68982e14cdfa292b180f9a6
tryadnancode <adnanansari1037@gmail.com>
2024-06-03 17:02:07 +0530

    Initial Commit

--- app/src/main/java/com/example/flipkart/MainActivity.java
+++ app/src/main/java/com/example/flipkart/MainActivity.java
@@ -1,49 +1,74 @@
 package com.example.flipkart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView,recent_recycler;
+    RecyclerView recyclerView, recent_recycler, suggest_recycler;
     ScrollAdapter adapter;
     RecentlyAdapter recent_adapter;
+    SuggestAdapter suggest_adapter;
+    TextView marqueeText1;
     Handler handler;
     Runnable runnable;
     int currentIndex = 0;
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView=findViewById(R.id.recyclerView);
-        recent_recycler=findViewById(R.id.recycler_recently_viewed);
+        findId();
+        BannerData();
+        SuggestData();
+        LayoutManager();
+        MarqueeScroll();
+        onClick();
+    }
 
-        TextView marqueeText1 = findViewById(R.id.marqueeText1);
+    private void onClick() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+                    if(itemId == R.id.account){
+                        Intent i = new Intent(MainActivity.this,Login.class);
+                        startActivity(i);
+                        return true;
+                }
+//                    if(itemId == R.id.home){
+//                        Intent i = new Intent(MainActivity.this,MainActivity.class);
+//                        startActivity(i);
+//                        return true;
+//                    }
+                    if(itemId == R.id.categories){
+                        Snackbar.make(findViewById(R.id.bottom_nav),"Categories",Snackbar.LENGTH_SHORT).show();
+                        return true;
+                    }
+                return false;
+            }
+        });
+    }
+
+    private void MarqueeScroll() {
         marqueeText1.setSelected(true);
-        List<BannerItem> bannerItems = new ArrayList<>();
-        bannerItems.add(new BannerItem(R.drawable.logo,R.drawable.account_circle));
-        bannerItems.add(new BannerItem(R.drawable.logo_bg,R.drawable.camera));
-        bannerItems.add(new BannerItem(R.drawable.logo,R.drawable.cart));
-        bannerItems.add(new BannerItem(R.drawable.down,R.drawable.mic));
-
-        adapter = new ScrollAdapter(this, bannerItems);
-        recyclerView.setAdapter(adapter);
-
-        recent_adapter = new RecentlyAdapter(this,bannerItems);
-        recent_recycler.setAdapter(recent_adapter);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView.setLayoutManager(layoutManager);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
-        recent_recycler.setLayoutManager(linearLayoutManager);
-
         handler = new Handler();
         runnable = new Runnable() {
             @Override
@@ -55,11 +80,56 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(this, 2000);
             }
         };
-
         handler.postDelayed(runnable, 1000);
     }
+
     protected void onDestroy() {
         super.onDestroy();
         handler.removeCallbacks(runnable);
+    }
+
+    private void LayoutManager() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recent_recycler.setLayoutManager(linearLayoutManager);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        suggest_recycler.setLayoutManager(gridLayoutManager);
+    }
+
+    private void SuggestData() {
+        List<SuggestItem> suggestItems = new ArrayList<>();
+        suggestItems.add(new SuggestItem(R.drawable.down));
+        suggestItems.add(new SuggestItem(R.drawable.down));
+        suggestItems.add(new SuggestItem(R.drawable.down));
+        suggestItems.add(new SuggestItem(R.drawable.down));
+        suggestItems.add(new SuggestItem(R.drawable.down));
+        suggestItems.add(new SuggestItem(R.drawable.down));
+        suggest_adapter = new SuggestAdapter(this, suggestItems);
+        suggest_recycler.setAdapter(suggest_adapter);
+    }
+
+    private void BannerData() {
+        List<BannerItem> bannerItems = new ArrayList<>();
+        bannerItems.add(new BannerItem(R.drawable.logo, R.drawable.account_circle));
+        bannerItems.add(new BannerItem(R.drawable.logo_bg, R.drawable.camera));
+        bannerItems.add(new BannerItem(R.drawable.logo, R.drawable.cart));
+        bannerItems.add(new BannerItem(R.drawable.down, R.drawable.mic));
+
+        adapter = new ScrollAdapter(this, bannerItems);
+        recyclerView.setAdapter(adapter);
+
+        recent_adapter = new RecentlyAdapter(this, bannerItems);
+        recent_recycler.setAdapter(recent_adapter);
+    }
+
+    private void findId() {
+        recyclerView = findViewById(R.id.recyclerView);
+        recent_recycler = findViewById(R.id.recycler_recently_viewed);
+        suggest_recycler = findViewById(R.id.suggest_recycler);
+        marqueeText1 = findViewById(R.id.marqueeText1);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
     }
 }
